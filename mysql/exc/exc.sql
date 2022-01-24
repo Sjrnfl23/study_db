01854e4d6d51f7e12a878e36870dcdcc793764c5
sjrnfl23 <tnsgud1010@m365.dongyang.ac.kr>
2022-01-24 11:52:21 +0900

    2

---
+++ mysql/exc/exc.sql
@@ -0,0 +1,20 @@
+
+
+show tables;
+
+-- 테이블 생성
+create table member(
+seq int,
+name varchar(100),
+id varchar(100),
+regdate datetime
+);
+
+create table member2(
+seq int,
+name varchar(100),
+id varchar(100),
+regdate datetime
+);
+-- 테이블 삭제
+drop table member;
